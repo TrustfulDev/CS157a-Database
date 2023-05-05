f6159ce17137bc5d99969487bfa84b7ee314feb8
Steven Vu <sssstevenvu@gmail.com>
2023-05-04 17:42:42 -0700

    Finishes navbar component
    Fully responsive!

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <header className="nav-container">
             <h1>Syntax Savants</h1>
 
-            <nav>
+            <nav className={ open ? "nav-open" : "nav-close"}>
                 <Link to="/" className="nav-btns">Something</Link>
                 <Link to="/" className="nav-btns">Something</Link>
                 <Link to="/" className="nav-btns">About Us</Link>
             </nav>
+
+            <div className="hamburger" onClick={() => setOpen(!open)}>
+                <div className={ open ? "ham-lines first-line" : "ham-lines"}></div>
+                <div className={ open ? "ham-lines sec-line" : "ham-lines"}></div>
+                <div className={ open ? "ham-lines third-line" : "ham-lines"}></div>
+            </div>
         </header>
     );
 };
