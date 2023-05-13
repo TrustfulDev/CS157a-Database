59cd43e1cb60a5386cc7d792b8e85315492783ad
Steven Vu <sssstevenvu@gmail.com>
2023-05-13 16:46:28 -0700

    fix picture typo

--- Client/src/assets/index.js
+++ Client/src/assets/index.js
@@ -1,3 +1,3 @@
 export { default as Hasnain } from './Hasnain.png';
-export { default as StevenVu } from './stevenVu.png';
+export { default as StevenVu } from './StevenVu.png';
 export { default as TommyVu } from './TommyVu.png';
